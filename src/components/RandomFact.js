0de1710572f0c834c6903e6ea27f92635e3cf519
Alanna <alanna@projectwalrus.com>
2020-06-13 03:09:01 -0300

    fix issue with german not populating german fact

--- src/components/RandomFact.js
+++ src/components/RandomFact.js
@@ -3,23 +3,24 @@
  * @description Displays a random fact from random APIS
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as PropTypes from 'prop-types';
 
+import { withRouter } from 'react-router-dom';
 import Messaging from './Messaging';
 
 import { getMessage } from '../redux/actions/messageAction';
+import parseQuery from '../hooks/parseQuery';
 
 const RandomFact = (props) => {
   const dispatch = useDispatch();
 
   const { message, type } = useSelector((state) => state.message);
 
-  const { language } = props;
-
   const randomFactRequest = () => {
-    dispatch(getMessage(language));
+    const query = props.location.search;
+    dispatch(getMessage(parseQuery(query).language));
   };
 
   return (
@@ -40,4 +41,4 @@ RandomFact.defaultProps = {
   classes: {},
 };
 
-export default RandomFact;
+export default withRouter(RandomFact);
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,21 +3,15 @@
  * @description Main home screen that displays different card elements
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import * as PropTypes from 'prop-types';
 import RandomFact from '../components/RandomFact';
 
-import parseQuery from '../hooks/parseQuery';
-
 import Dashboard from './Dashboard';
 
-const Main = (props) => {
-  const query = props.location.search;
-  const [language, setLanguage] = useState(parseQuery(query).language); // FIXME, this doesn't handle multiple query params
-
-  useEffect(() => { setLanguage(language); }, [language]);
-  const [confirmed, setConfirmed] = useState(false);
+const Main = () => {
+  const [hideMessage, setHideMessage] = useState(false);
 
   return (
     <Dashboard>
@@ -31,9 +25,9 @@ const Main = (props) => {
         <Grid item xs={12} md={10}>
           <Grid item xs={12}><Typography variant="h3">Welcome</Typography></Grid>
 
-          {confirmed === false
+          {hideMessage === false
           && (
-            <RandomFact language={language} response={(response) => { setConfirmed(response); }} />)}
+            <RandomFact response={(response) => { setHideMessage(response); }} />)}
 
         </Grid>
       </Grid>
